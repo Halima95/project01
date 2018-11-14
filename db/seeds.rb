f81037247a080e0e33c16d0085a7268296765255
Halima <Halimaalshahrani@gmail.com>
2018-11-14 10:01:11 +0300

    Last commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ dhahran = City.create(name: "Dhahran", image: "https://doubletree3.hilton.com/re
 
 abha = City.create(name: "Abha", image: "https://pbs.twimg.com/media/ClAticeWAAE7sdf.jpg:large")
 
-almadinah = City.create(name: "Almadinah Almunawarah", image: "http://www.travelnostop.com/wp-content/uploads/2018/01/9108_saudi_arabia_medina_al-masjid_al-nabawi_thumbnails-610x366.jpg")
+almadinah = City.create(name: "Almadinah", image: "http://www.travelnostop.com/wp-content/uploads/2018/01/9108_saudi_arabia_medina_al-masjid_al-nabawi_thumbnails-610x366.jpg")
 
 
 
@@ -44,14 +44,15 @@ green = Place.create(name: "Green Mountain", location: "https://goo.gl/maps/mcTx
 
 alula = Place.create(name: "AlUla", location: "https://goo.gl/maps/UQcmRvaA7r72", city_id: almadinah.id, image: "https://assets.atlasobscura.com/media/W1siZiIsInVwbG9hZHMvcGxhY2VfaW1hZ2VzLzQ1OWQzZmI5ZGQzNTNmZWQyMV80MDA5OTE1OTU3XzAzMzE1NWYyNjJfby5qcGciXSxbInAiLCJ0aHVtYiIsIjEyMDB4PiJdLFsicCIsImNvbnZlcnQiLCItcXVhbGl0eSA4MSAtYXV0by1vcmllbnQiXV0/4009915957_033155f262_o.jpg")
 
-alaan= Place.create(name: "Alaan Artspace", location: "https://goo.gl/maps/bxqqmoXwp5w", city_id: riyadh.id, image: "http://alaanart.com/uploads/crop_image/428/383/20140409223715_aboutalaan.png?x=-56&y=-101&w=683&o=428?")
+alaan= Place.create(name: "Alāan Artspace", location: "https://goo.gl/maps/bxqqmoXwp5w", city_id: riyadh.id, image: "http://alaanart.com/uploads/crop_image/428/383/20140409223715_aboutalaan.png?x=-56&y=-101&w=683&o=428?")
 
+almiftaha = Place.create(name: "AlMiftaha village", location: "https://goo.gl/maps/nH1PtaGSH1p", city_id: abha.id, image: "https://www.almowaten.net/wp-content/uploads/2018/09/AF964EF6-B2D8-4648-9F08-3CC5D9C6C7FA-697x800.jpeg")
 
 
 
 albujairi = Activity.create(type_of: "Albujairi", description: "Al Bujairi is a modern development in diriyah with an overview on preserved areas of Old diriyah.. it has many Saudi brands including restaurants, Cafes and souvenir shops. It would be nice to go when the weather is cooler and maybe have a traditional Saudi dinner.", age_rating: "Suitable for Youth, Teens, Adult", gender: "Female and male", place_id: diriyah.id, category_id: (Category.find_by(name: "Parks").id))
 
-diving = Activity.create(type_of: "Diving center", description: "Provided a very special services and traning packages with excellent instructors.", age_rating: "20+", gender: "Male", place_id: dive.id, category_id: (Category.find_by(name: "Diving").id))
+diving = Activity.create(type_of: "Dammam Diving Center", description: "Provided a very special services and traning packages with excellent instructors.", age_rating: "20+", gender: "Male", place_id: dive.id, category_id: (Category.find_by(name: "Diving").id))
 
 albalad_ = Activity.create(type_of: "Historical jeddah", description: "Beautiful, intricately designed old merchant houses can be found all over the area, the most famous being Al Naseef House (Beit al Nassef). This stunning building has been fully preserved and is now open to public as a museum. Another museum worth visiting is the Jeddah Municipality Museum. The very first school-now museum, in the entire Arabian peninsula is located in Al Balad (Madrasa Al Falah).", age_rating: "Suitable for Youth, Teens, Adult", gender: "Female and male", place_id: albalad.id, category_id: (Category.find_by(name: "Historical").id))
 
@@ -65,11 +66,11 @@ madain = Activity.create(type_of: "Mada’in Saleh", description: "The historic 
 
  ithraaMovie = Activity.create(type_of: "Robotic Art: Transformations", description: "Interviews filmed at the Robotic Arts Exhibition presented by the Cité des Sciences et de l'industrie in Paris, (8 April 2014 - 4 January 2015). ROBOTIC ART exhibition looks at transformations made possible by the use of technologies developed for the most part since the mid-twentieth century, particularly electronics, computing, bionics, and robotics. Genre: Documentary Classification: G Year: 2017 Duration: 38 mins Curator: Richard Castelli", age_rating: "Suitable for Kids", gender: "Female and male", place_id:ithraa.id, category_id:(Category.find_by(name: "Theatre").id ))
 
-green_ = Activity.create(type_of: "Cable car", description: "The “mountain” is actually a hill overlooking the city which is beautifully lit with green lights in the night. There’s a restaurant and café on the top with a nice outdoor terrace enjoying amazing views of Abha, and you can take a cable car ride from/to the mountain.", age_rating: "Suitable for Youth, Teens, Adult", gender: "Families", place_id: green.id, category_id: (Category.find_by(name: "Parks").id))
+green_ = Activity.create(type_of: " Green Mountain (Cable car)", description: "The “mountain” is actually a hill overlooking the city which is beautifully lit with green lights in the night. There’s a restaurant and café on the top with a nice outdoor terrace enjoying amazing views of Abha, and you can take a cable car ride from/to the mountain.", age_rating: "Suitable for Youth, Teens, Adult", gender: "Families", place_id: green.id, category_id: (Category.find_by(name: "Parks").id))
 
-arch = Activity.create(type_of: "AlMiftaha village", description: "Discover Asir regions traditional architecture by visiting AlMiftaha village turned museum to see how people lived back in the day.", age_rating: "Suitable for Youth, Teens, Adult", gender: "Families", place_id: green.id, category_id:( Category.find_by(name: "Historical").id))
+arch = Activity.create(type_of: "Asir traditional architecture", description: "Discover Asir regions traditional architecture by visiting AlMiftaha village turned museum to see how people lived back in the day.", age_rating: "Suitable for Youth, Teens, Adult", gender: "Families", place_id: almiftaha.id, category_id:( Category.find_by(name: "Historical").id))
 
-art = Activity.create(type_of: " Alāan ", description: "Alāan – which means ‘now’ in Arabic – reflects the energy of the art scene in Saudi Arabia and the feeling in Riyadh that a space such as this is long overdue. Is a multi-functional contemporary gallery, educational hub, library, restaurant, shop and coffeehouse located in the heart of Riyadh. Dedicated to nurturing emerging and established contemporary artists and designers from Saudi Arabia, the region and across the globe. Alāan Artspace’s programme offers a platform for curated exhibitions and a non-profit educational forum for artists, creative practitioners and art enthusiasts of all ages.", age_rating: "Suitable for Youth, Teens, Adult", gender: "Female and male", place_id: diriyah.id, category_id: (Category.find_by(name: "Art").id))
+art = Activity.create(type_of: " Alāan ", description: "Alāan – which means ‘now’ in Arabic – reflects the energy of the art scene in Saudi Arabia and the feeling in Riyadh that a space such as this is long overdue. Is a multi-functional contemporary gallery, educational hub, library, restaurant, shop and coffeehouse located in the heart of Riyadh. Dedicated to nurturing emerging and established contemporary artists and designers from Saudi Arabia, the region and across the globe. Alāan Artspace’s programme offers a platform for curated exhibitions and a non-profit educational forum for artists, creative practitioners and art enthusiasts of all ages.", age_rating: "Suitable for Youth, Teens, Adult", gender: "Female and male", place_id: alaan.id, category_id: (Category.find_by(name: "Art").id))
 
 
 # albujairi.category << Category.find_by(name: "Parks")
